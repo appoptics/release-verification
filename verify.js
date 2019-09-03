eeaa4a54daeb2bea56f35a31e40e7fecdc709f43
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-09-03 13:53:34 -0700

    meandering along

--- verify.js
+++ verify.js
@@ -1,22 +1,31 @@
 'use strict'
 
-const axios = require('axios');
 const fs = require('fs');
+const axios = require('axios');
+const semver = require('semver');
 
-const options = getCommandLineOptions(process.argv);
+const {options, showHelp} = require('./lib/get-cli-options');
+
+if (options.help) {
+  showHelp();
+  return;
+}
 
 const packageMap = {
   node: {
+    name: 'appoptics-apm',
     releases: 'https://registry.npmjs.com/appoptics-apm',
     gitRepo: 'appoptics/appoptics-apm-node',
+    tagTemplate: 'v${tag}',
   }
 }
 
 let repoInfo;
 let p;
+let p2;
 let gitRepo;
 
-if (options.package === 'node') {
+if (options.agent === 'node') {
   const releaseUrl = packageMap.node.releases;
   gitRepo = packageMap.node.gitRepo;
   p = axios.get(releaseUrl)
@@ -34,24 +43,50 @@ if (options.package === 'node') {
 }
 
 let tag;
-let writer;
 
+// here p is a promise resolving to (possibly normalized) meta
+// information about the package.
 p
   .then(() => {
+    // repoInfo.versions['6.7.0-rc3'].dist.tarball is npm
+    // version of tarball
     const versions = repoInfo.versions;
+
+    // get versions for verification
     const versionStrings = Object.keys(versions);
-    const lastVersion = versionStrings[versionStrings.length - 1];
-    console.log(`last version ${lastVersion}`);
-    console.log('keys', Object.keys(versions[lastVersion]));
-    tag = `v${lastVersion}`;
+
+    const versionsToVerify = selectVersions(versionStrings, options.versions);
+
+    if (options.info) {
+      console.log(`versions to verify: ${[versionsToVerify.join(',')]}`);
+    }
+
+    if (versionsToVerify.length === 0) {
+      throw new TypeError('No versions to verify');
+    }
+
+    // here needs to begin sequential processing of multiple versions
+    // when that gets implemented.
+    p2 = verifyThese(versionsToVerify.slice(0));
+
+    if (versionsToVerify.length !== 1) {
+      console.log(`only verifying ${versionsToVerify.slice(-1)}`);
+    }
+
+
+    tag = packageMap.node.tagTemplate.replace('${tag}', versionsToVerify.slice(-1));
     const tarballUrl = `https://api.github.com/repos/${gitRepo}/tarball/${tag}`
-    console.log(`fetching tarball url ${tarballUrl}`);
-    const options = {
+
+    if (options.info) {
+      console.log(`fetching tarball url ${tarballUrl}`);
+    }
+
+    const axiosOptions = {
       url: tarballUrl,
       method: 'GET',
       responseType: 'stream',
     }
-    return axios(options);
+    return axios(axiosOptions);
   })
   .then(r => {
     const writer = fs.createWriteStream(`appoptics-apm-${tag}.tar.gz`)
@@ -63,16 +98,36 @@ p
     })
   })
   .catch(e => {
-    console.log(e.message);
+    console.log(e.message, e.stack);
   })
   .then(r => {
+    return p2;
+  }).then(r => {
     console.log('finished');
   })
 
 
+// reducing promises sequentially
+function verifyThese (versions) {
+  return versions.reduce((p, version) => {
+    return p.then(() => verify(version));
+  }, Promise.resolve()); // initial
+};
 
-// https://api.github.com/repos/$REPOSITORY_NAME/tarball/$COMMI‌​T_ID
-
-function getCommandLineOptions (argv) {
-  return {package: 'node'};
+// fake function to pretend to verify
+function verify (version) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('[pretend] executed version', version);
+      resolve();
+    }, 250);
+  });
 }
+
+function selectVersions (versions, requested) {
+  if (requested === 'latest') {
+    return versions.slice(-1);
+  }
+  return versions.filter(version => semver.satisfies(version, requested));
+}
+// https://api.github.com/repos/$REPOSITORY_NAME/tarball/$COMMI‌​T_ID
