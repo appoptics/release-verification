27323eb7c0aa8b84571eb5e5025e8fc729b1c9e7
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-09-04 07:16:04 -0700

    continuing
    - make download()
    - download npm package too
    - put packages in npm/ and git/

--- verify.js
+++ verify.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const fs = require('fs');
+
+const mkdirp = require('mkdirp');
 const axios = require('axios');
 const semver = require('semver');
 
@@ -15,10 +17,15 @@ const packageMap = {
   node: {
     name: 'appoptics-apm',
     releasesUrl: 'https://registry.npmjs.com/appoptics-apm',
-    gitRepo: 'appoptics/appoptics-apm-node',
+    gitUser: 'appoptics',
+    gitRepo: 'appoptics-apm-node',
     tagTemplate: 'v${tag}',
   }
 }
+
+// make places to put the downloads.
+mkdirp.sync('npm');
+mkdirp.sync('git');
 
 let repoInfo;
 let p;
@@ -36,10 +43,13 @@ if (options.agent === 'node') {
       return repoInfo;
     })
     .catch(e => {
-      console.log(`failed to get ${releaseUrl}:`, e);
+      console.log(`failed to get ${agentMap.releasesUrl}:`, e);
       throw e;
     })
 } else {
+  // The gems go to rubygems.org.
+  // https://rubygems.org/api/v1/gems/appoptics_apm.json
+  // The oboe version is packaged with the gem in ext/oboe_metal/src/VERSION.
   throw new Error('only the node package is implemented');
 }
 
@@ -69,6 +79,8 @@ p.then(() => {
   return verifyThese(versionsToVerify).then(() => versionsToVerify);
 }).then(results => {
   console.log(results);
+}).catch(e => {
+  console.log('unexpected error', e.message, e.stack);
 })
 
 // execute promises sequentially with reduce. the argument "versions"
@@ -78,10 +90,10 @@ function verifyThese (versions) {
     return p
       .then(r => {
         return verify(version)
-          .then(r => {versions[ix] = {version: r, status: 'good'}});
+          .then(r => {versions[ix] = {version, status: 'good', bytes: r}});
       })
       .catch(e => {
-        versions[ix] = {version: r, status: 'error', error: e};
+        versions[ix] = {version, status: 'error', error: e};
       })
   }, Promise.resolve()).then(() => versions);
 };
@@ -89,37 +101,50 @@ function verifyThese (versions) {
 // verify the specified version
 function verify (version) {
   const tag = agentMap.tagTemplate.replace('${tag}', version);
+  const npmUrl = repoInfo.versions[version].dist.tarball;
+  const npmTarget = `npm/npm-${tag}.tar.gz`;
+  const gitUrl = `https://api.github.com/repos/${agentMap.gitUser}/${agentMap.gitRepo}/tarball/${tag}`;
+  const gitTarget = `git/${agentMap.gitRepo}-${tag}.tar.gz`;
 
-  return new Promise((resolve, reject) => {
-    // get the git tarball for the version
-    const tarballUrl = `https://api.github.com/repos/${agentMap.gitRepo}/tarball/${tag}`;
-
-    if (options.info) {
-      console.log(`fetching tarball url ${tarballUrl}`);
-    }
-
-    const axiosOptions = {
-      url: tarballUrl,
-      method: 'GET',
-      responseType: 'stream',
-    }
-
-    return axios(axiosOptions)
-      .then(r => {
-        const writer = fs.createWriteStream(`appoptics-apm-${tag}.tar.gz`)
-        r.data.pipe(writer);
-        writer.on('finish', () => {resolve(version)});
-        writer.on('error', reject);
-      })
-      .catch(reject);
-  })
+  return download(npmUrl, npmTarget)
+    .then(npmBytes => {
+      return download(gitUrl, gitTarget)
+        .then(gitBytes => {
+          return {npmBytes, gitBytes};
+        })
+    })
+    .catch(e => {
+      throw e;
+    })
+    .then(bytes => {
+      // unpack both files
+      // walk through npm files (subset of git files) and compare
+      // - first size, then contents
+      return bytes;
+    })
 }
 
 //
 // download a file at a given url to a specified name
 //
 function download (url, name) {
+  return new Promise((resolve, reject) => {
 
+    const axiosOptions = {
+      url,
+      method: 'GET',
+      responseType: 'stream',
+    }
+
+    axios(axiosOptions)
+      .then(r => {
+        const writer = fs.createWriteStream(name)
+        r.data.pipe(writer);
+        writer.on('finish', () => {resolve(writer.bytesWritten)});
+        writer.on('error', reject);
+      })
+      .catch(reject);
+  })
 }
 
 //
