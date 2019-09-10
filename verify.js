378c2d96e26b6b68d437f8c59eb051e339414157
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-09-09 20:57:49 -0700

    verify.js - improve error handling

--- verify.js
+++ verify.js
@@ -11,6 +11,11 @@ if (options.help || options._.length !== 1) {
   return;
 }
 
+const greenOn = '\x1b[38;5;10m';
+const greenOff = '\x1b[m';
+const redOn = '\x1b[38;5;9m';
+const redOff = '\x1b[m';
+
 // use pkg rather than package because it's reserved by javascript.
 const pkg = options._[0];
 const {versions, repository, info, differences, simulate, exclude} = options;
@@ -25,25 +30,27 @@ async function main () {
   if (!verifier) {
     const valid = Object.keys(verifiers).join(', ');
     // eslint-disable-next-line no-console
-    console.error(`repository ${repository} not supported. valid: ${valid}`);
-    return;
+    console.error(`${redOn}repository ${repository} not supported.${redOff} valid: ${valid}`);
+    return 0;
   }
   const nv = await verifier(pkg, {info, differences, simulate});
 
   const gmvOptions = options['include-prerelease'] ? {includePrerelease: true} : {};
   const versionList = nv.getMatchingVersions(versions, gmvOptions);
+  if (versionList.length === 0) {
+    // eslint-disable-next-line no-console
+    console.error(`${redOn}no versions matching ${versions}${redOff}`);
+    return 0;
+  }
+
   const results = await nv.verifyThese(versionList);
 
   // eslint-disable-next-line no-console
   console.log('');
 
-  const greenOn = '\x1b[38;5;10m';
-  const greenOff = '\x1b[m';
   const greenCheck = `${greenOn}✓${greenOff} `;
   const type = differences === 'all' ? '' : 'important ';
   const noDiffsMessage = `${greenCheck}no ${type}differences \${version}`;
-  const redOn = '\x1b[38;5;9m';
-  const redOff = '\x1b[m ';
 
   let status = 0;
   results.forEach(result => {
