5eb95e4219bcbe44d6b6153cf7cbdd3939605008
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-09-09 19:35:01 -0700

    node, ruby support
    - allow verifying prerelease versions
    - nicer output (room for improvement remains)
    - more consistent error handling
    - cleanup dot files too
    - add exclude, include-prerelease options
    - finish ruby support, initial release

--- verify.js
+++ verify.js
@@ -11,40 +11,64 @@ if (options.help || options._.length !== 1) {
   return;
 }
 
-// use p rather than the long name package because it's reserved by javascript.
+// use pkg rather than package because it's reserved by javascript.
 const pkg = options._[0];
-const {versions, repository, info, differences} = options;
+const {versions, repository, info, differences, simulate, exclude} = options;
 
-const verifier = {
+const verifiers = {
   npm: makeNpmVerifier,
-  ruby: makeRubyVerifier,
+  rubygems: makeRubyVerifier,
 }
 
 async function main () {
-  const nv = await verifier[repository](pkg, {info, differences});
+  const verifier = verifiers[repository];
+  if (!verifier) {
+    const valid = Object.keys(verifiers).join(', ');
+    // eslint-disable-next-line no-console
+    console.error(`repository ${repository} not supported. valid: ${valid}`);
+    return;
+  }
+  const nv = await verifier(pkg, {info, differences, simulate});
 
-  const versionList = nv.getMatchingVersions(versions);
+  const gmvOptions = options['include-prerelease'] ? {includePrerelease: true} : {};
+  const versionList = nv.getMatchingVersions(versions, gmvOptions);
   const results = await nv.verifyThese(versionList);
 
   // eslint-disable-next-line no-console
   console.log('');
 
+  const greenOn = '\x1b[38;5;10m';
+  const greenOff = '\x1b[m';
+  const greenCheck = `${greenOn}✓${greenOff} `;
+  const type = differences === 'all' ? '' : 'important ';
+  const noDiffsMessage = `${greenCheck}no ${type}differences \${version}`;
+  const redOn = '\x1b[38;5;9m';
+  const redOff = '\x1b[m ';
+
   let status = 0;
   results.forEach(result => {
-    const diffResults = nv.extractDifferences(result, {differences});
-    if (diffResults) {
+    if (result.status === 'fatal') {
       // eslint-disable-next-line no-console
-      console.log(`unexpected differences for ${result.version}:\n${diffResults.join('\n')}`);
-      status = 1;
+      console.log(result.error);
+    } else if (result.status === 'error') {
+      const diffResults = nv.extractDifferences(result, {differences, exclude});
+      // there can be differences that aren't important
+      if (!diffResults) {
+        // eslint-disable-next-line no-console
+        console.log(noDiffsMessage.replace('${version}', result.version));
+      } else {
+
+        // eslint-disable-next-line no-console
+        console.log(`${redOn}? ${differences} differences for ${result.version}:\n${diffResults.join('\n')}${redOff}`);
+        status = 1;
+      }
+    } else if (result.status === 'good') {
+      // eslint-disable-next-line no-console
+      console.log(noDiffsMessage.replace('${version}', result.version));
     }
   })
   return status;
 }
-
-// The gems go to rubygems.org.
-// https://rubygems.org/api/v1/gems/appoptics_apm.json
-// The oboe version is packaged with the gem in ext/oboe_metal/src/VERSION.
-
 
 main().then(r => {
   const msg = r === 0 ? 'done' : 'differences';
