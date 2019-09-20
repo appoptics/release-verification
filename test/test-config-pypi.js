b00aecf05a30ea4898b52c593bf88e908acb35ac
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-09-19 20:52:44 -0700

    create test-config-pypi.js

---
+++ test/test-config-pypi.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  excludeFile: [
+    'appoptics_apm.egg-info',
+    'PKG-INFO',
+    'setup.cfg'
+  ],
+  excludeDir: [
+    'appoptics_apm/swig'
+  ],
+  noWarn: true,
+}
